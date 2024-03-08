cc1f4ac7fcee0e56a42969e48a7d8f451d93fe28
Jordan Cox <jordancox747@outlook.com>
2024-03-08 10:46:18 -0700

    Form validation working with zod

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,33 +1,48 @@
 "use client";
 
 import { FormEvent, useState } from "react";
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from "zod";
+
+const loginSchema = z.object({  
+  email: z.string().email({ message: 'Invalid email format' }),  
+  password: z.string().min(8, { message: 'Password should be at least 8 characters long'} ),
+});
 
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleSubmit = async (e: FormEvent) => {
-    e.preventDefault();
+  const onSubmit = async (data: any) => {
     // Your login logic here, e.g., making a request to your API route
-
+    console.log(data)
   };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(loginSchema),
+  });
 
   return (
     <>
     <h1 className="text-center text-4xl font-medium pb-12">Login</h1>
     <div className="flex items-center justify-center min-h-full pb-12">
       <div className="bg-white shadow-md rounded px-8 py-6 w-full max-w-md text-black">
-        <form onSubmit={handleSubmit} className="space-y-4">
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
           <label htmlFor="email" className="block text-sm font-medium">
             Email:
           </label>
           <input
             type="email"
             id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            {...register("email")}
             className="w-full px-3 py-2 rounded-md border border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-500"
           />
+          {errors.email && <p className="text-red-500">{errors.email.message?.toString()}</p>}
 
           <label htmlFor="password" className="block text-sm font-medium">
             Password:
@@ -35,10 +50,11 @@ export default function Login() {
           <input
             type="password"
             id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            {...register("password")}
             className="w-full px-3 py-2 rounded-md border border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-500"
           />
+          {errors.password && <p className="text-red-500">{errors.password.message?.toString()}</p>}
+
           <button
             type="submit"
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-700"
